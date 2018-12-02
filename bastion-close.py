3e08a5c694955dcedc85e9c9c2bf1acaf2525738
EC2 Default User <ec2-user@ip-172-31-30-117.us-west-2.compute.internal>
2018-12-02 07:54:05 +0000

    first commit

---
+++ bastion-close.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import boto3
+
+GROUP_NAME = "Bastion"
+
+# Connect to the Amazon EC2 service
+ec2 = boto3.resource('ec2')
+
+# Retrieve the security group
+security_groups = ec2.security_groups.filter(GroupNames=[GROUP_NAME])
+
+# Delete all rules in the group
+for group in security_groups:
+    group.revoke_ingress(IpPermissions = group.ip_permissions)
